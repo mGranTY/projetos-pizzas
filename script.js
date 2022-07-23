66be8891eda11a39d6cb18b8cda8c27d12723c7c
GranTY <grantygames@gmail.com>
2022-07-22 22:41:39 -0400

    Preencher site com as pizzas e suas caracteriscas

---
+++ script.js
@@ -0,0 +1,21 @@
+// const listarPizzas = () => {
+//     let divHtml = document.getElementsByClassName('pizza-item');
+//     console.log(JSON.parse(JSON.stringify(divHtml)))
+// }
+//
+// listarPizzas();
+
+
+const listarPizzas = () => {
+    pizzaJson.map( (pizzas, index) => {
+        let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+        //console.log(pizzaItem)
+        // console.log(pizzas, index)
+        pizzaItem.querySelector('.pizza-item--img').innerHTML = `<img src="${pizzas.img}" />`
+        pizzaItem.querySelector('.pizza-item--price').innerHTML = 'R$ '+pizzas.price.toFixed(2);
+        pizzaItem.querySelector('.pizza-item--name').innerHTML = pizzas.name;
+        pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizzas.description;
+        document.querySelector('.pizza-area').append(pizzaItem)
+    } )
+}
+listarPizzas();
